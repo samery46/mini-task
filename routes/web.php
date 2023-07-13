d7b184ccd2a69157f92f4cf6b4b01ddfff737928
samery46 <syamme@ymail.com>
2023-07-13 22:15:56 +0700

    Develop Edit Update

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,4 @@ Route::get('/', function () {
 });
 
 
-Route::prefix('blog')
-    ->name('blog.')
-    ->controller(BlogController::class)
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');  // Ditambahkan        
-        Route::get('/{id}/edit', 'edit')->name('edit');
-        Route::put('/{id}', 'update')->name('update');
-        Route::get('/{id}/delete', 'delete')->name('delete');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('progress', 'progress')->name('progress');
-        Route::patch('{id}/move', 'move')->name('move');
-        Route::patch('{id}/complete', 'complete')->name('complete');
-    });
+Route::resource('blog', BlogController::class);
