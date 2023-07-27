1f24abb7e628698333e12ee651738a6ef94a1889
samery46 <syamme@ymail.com>
2023-07-28 06:58:42 +0700

    Develop Logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/blog';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
